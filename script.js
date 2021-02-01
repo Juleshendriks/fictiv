fe90d07324b8b75a19827c1204accdd2b7b62d48
Jules Hendriks <jules.jhendriks@gmail.com>
2021-02-01 23:25:07 +0100

    first upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const scrollBtn = document.getElementById("backBtn");
+const menuBtn = document.getElementById("menuBtn");
+const menuDiv = document.getElementById("menuDiv");
+const body =   document.getElementById("body");
+
+const logo = document.getElementById("logo");
+
+function scrollTop(){
+    
+  console.log("Scrolling");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+}
+
+scrollBtn.addEventListener('click', () => {
+  scrollTop();
+});
+
+menuBtn.addEventListener('click', () => {
+  logo.classList.toggle("wideLogo");
+  menuBtn.classList.toggle("rotate");
+  menuDiv.classList.toggle("showMenu");
+  body.classList.toggle("noFlow");
+});
